95c03c1c49e692f2884ea3efdddd017e2f690b37
Katie  Lin <Katie@Katies-MacBook-Air.local>
2015-08-19 14:09:00 -0700

    Giffaw complete

--- base.js
+++ base.js
@@ -1,7 +1,42 @@
+//var input = $("input").val();
+
 $(document).ready(function(){
   console.log("here's your mock data to start with:")
   console.log(GLOBAL_MOCK_DATA_OBJECT)
 
+  //GLOBAL_MOCK_DATA_OBJECT.data.forEach(function(element, index){
+  	//var url = element.images.fixed_height.url;
+  	//$("#sanity-check").append("<img src='" + url + "'>");
+  	
+  	$.get("http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC", function handleResponse(data){
+  		data.data.forEach(function(element){
+  			$("#sanity-check").append("<img src='" + element.images.fixed_height.url + "'>");
+  		});
+
+	});
+
+  	$(window).keydown(function(e){
+		//alert("key was pressed! ");
+        var keyCode = e.keyCode;
+    	//console.log(keyCode);
+        if(keyCode === 13)
+        {
+        	 $("img").remove();
+        	//var input = $("input").val();
+  			$.get("http://api.giphy.com/v1/gifs/search",{q:$("input").val(), api_key:"dc6zaTOxFJmzC"}, function handleResponse(data){
+  				data.data.forEach(function(element){
+  					$("#sanity-check").append("<img src='" + element.images.fixed_height.url + "'>");
+  				});
+			});
+      
+        }     
+    });
+
+    
+  //});
+ 
   // can you render the mock data to the page?
 
 })
+
+
